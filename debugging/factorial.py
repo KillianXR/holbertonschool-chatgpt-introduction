52334b558893679a56e31cfe3c98f1e6bdd7bdd5
KillianXR <9756@holbertonstudents.com>
2025-01-13 22:10:58 +0100

    debugging 2

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 import sys
 
+# Vérifier s'il y a un argument passé en ligne de commande
+if len(sys.argv) < 2:
+    print("Usage: python3 factorial.py <number>")
+    sys.exit(1)
+
 def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Réduction de n à chaque itération
+        n -= 1
     return result
 
 f = factorial(int(sys.argv[1]))
