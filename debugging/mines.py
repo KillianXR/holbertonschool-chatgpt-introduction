2762da96f3c347d0c8b9a2d0bc42fc63d48b4ac4
KillianXR <9756@holbertonstudents.com>
2025-01-14 12:36:22 +0100

    debug

--- debugging/mines.py
+++ debugging/mines.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import random
 import os
 
@@ -11,7 +12,6 @@ class Minesweeper:
         self.mines = set(random.sample(range(width * height), mines))
         self.field = [[' ' for _ in range(width)] for _ in range(height)]
         self.revealed = [[False for _ in range(width)] for _ in range(height)]
-        self.safe_cells = self.width * self.height - len(self.mines)  # Total safe cells
 
     def print_board(self, reveal=False):
         clear_screen()
@@ -42,20 +42,24 @@ class Minesweeper:
     def reveal(self, x, y):
         if (y * self.width + x) in self.mines:
             return False
-        if self.revealed[y][x]:
-            return True
         self.revealed[y][x] = True
-        self.safe_cells -= 1
         if self.count_mines_nearby(x, y) == 0:
             for dx in [-1, 0, 1]:
                 for dy in [-1, 0, 1]:
                     nx, ny = x + dx, y + dy
-                    if 0 <= nx < self.width and 0 <= ny < self.height:
-                        self.reveal(nx, ny)  # Recursive reveal
+                    if 0 <= nx < self.width and 0 <= ny < self.height and not self.revealed[ny][nx]:
+                        self.reveal(nx, ny)
         return True
 
-    def valid_coordinates(self, x, y):
-        return 0 <= x < self.width and 0 <= y < self.height
+    def check_win(self):
+        # Count the number of non-mine, revealed cells
+        revealed_cells = sum(
+            1 for y in range(self.height) for x in range(self.width)
+            if self.revealed[y][x] and (y * self.width + x) not in self.mines
+        )
+        # The player wins if the number of revealed non-mine cells equals total non-mine cells
+        total_non_mine_cells = self.width * self.height - len(self.mines)
+        return revealed_cells == total_non_mine_cells
 
     def play(self):
         while True:
@@ -63,16 +67,13 @@ class Minesweeper:
             try:
                 x = int(input("Enter x coordinate: "))
                 y = int(input("Enter y coordinate: "))
-                if not self.valid_coordinates(x, y):
-                    print("Please enter valid coordinates within the bounds of the board.")
-                    continue
                 if not self.reveal(x, y):
                     self.print_board(reveal=True)
                     print("Game Over! You hit a mine.")
                     break
-                if self.safe_cells == 0:
+                if self.check_win():
                     self.print_board(reveal=True)
-                    print("Congratulations! You've won the game.")
+                    print("Congratulations! You've revealed all non-mine cells and won!")
                     break
             except ValueError:
                 print("Invalid input. Please enter numbers only.")
--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-# Start from index 1 to avoid printing the script name (sys.argv[0])
-for arg in sys.argv[1:]:
-    print(arg)
+# Start from index 1 to skip the script name
+for i in range(1, len(sys.argv)):
+    print(sys.argv[i])
